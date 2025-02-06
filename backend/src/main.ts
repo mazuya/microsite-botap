9fef61f3722dc856008fa684536806ffd3e4e892
T0Nattamate <nattamate.t@mobileconnect.co.th>
2025-02-06 11:45:37 +0700

    add prisma and enableCors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: true, // Allow requests from your frontend's origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(
+    process.env.PORT !== undefined && process.env.PORT !== null
+      ? process.env.PORT
+      : 3001,
+  );
 }
 bootstrap();
